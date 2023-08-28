9f38eade70875904f33764f64a19ac744424b20c
amisu-ctr <hamisurahman@gmail.com>
2023-08-28 18:07:18 +0000

    routing

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,22 +1,19 @@
 const express = require('express');
 const app = express();
 
+const people = require('./routes/people');
+const auth = require('./routes/auth');
+
 // static assets
 app.use(express.static('./methods-public'));
-//parse form data
+// parse form data
 app.use(express.urlencoded({ extended: false }));
 // parse json
 app.use(express.json());
 
-app.post('/login', (req, res) => {
-  console.log(req.body); //set urlecoded middleware to access form values
-  const { name } = req.body;
-  if (name) {
-    return res.status(200).send(`Welcome ${name} `);
-  }
-  res.status(401).send('Enter Credentials');
-});
+app.use('/api/people', people);
+app.use('/login', auth);
 
 app.listen(5000, () => {
-  console.log('listening to port 5000');
+  console.log('Server is listening on port 5000....');
 });
--- 02-express-tutorial/routes/people.js
+++ 02-express-tutorial/routes/people.js
@@ -1,63 +1,21 @@
 const express = require('express');
-
 const router = express.Router();
-let people = require('../data');
+const {
+  getPeople,
+  createPersonPostman,
+  createPerson,
+  updatePerson,
+  deletePerson,
+} = require('../controllers/people');
 
-router.get('/api/people', (req, res) => {
-  res.status(200).json({ success: true, data: people });
-});
+router.get('/', getPeople);
 
-router.post('/api/people', (req, res) => {
-  const { name } = req.body;
-  if (!name) {
-    return res
-      .status(400)
-      .json({ success: false, msg: 'please provide name value' });
-  }
-  res.status(201).send({ success: true, person: name });
-});
+router.post('/', createPerson);
 
-router.post('/api/postman/people', (req, res) => {
-  const { name } = req.body;
-  if (!name) {
-    return res
-      .status(400)
-      .json({ succes: false, msg: 'plesase provide name value' });
-  }
-  res.status(201).send({ success: true, data: [...people, name] });
-});
+router.post('/postman', createPersonPostman);
 
-router.put('/api/people/:id', (req, res) => {
-  const { id } = req.params;
-  const { name } = req.body;
-  console.log(id, name);
+router.put('/:id', updatePerson);
 
-  const person = people.find((person) => person.id === Number(id));
-  if (!person) {
-    return res
-      .status(404)
-      .json({ success: false, msg: `no person with id ${id}` });
-  }
-  const newPeople = people.map((person) => {
-    if (person.id === Number(id)) {
-      person.name = name;
-    }
-    return person;
-  });
-  res.status(200).json({ succes: true, data: newPeople });
-});
+router.delete('/:id', deletePerson);
 
-router.delete('/api/people/:id', (req, res) => {
-  const person = people.find((person) => person.id === Number(req.params.id));
-  if (!person) {
-    return res
-      .status(404)
-      .json({ success: false, msg: `no person with id ${req.params.id}` });
-  }
-  const newPeople = people.filter(
-    (person) => person.id !== Number(req.params.id)
-  );
-  return res.status(200).json({ success: true, data: newPeople });
-});
-
-module.exports.router;
+module.exports = router;
