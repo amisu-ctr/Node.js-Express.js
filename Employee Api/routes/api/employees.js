8562403518fab303575be9b2a1187977b796bdf5
amisu <hamisurahman@gmail.com>
2023-11-19 07:51:28 +0000

    Protect employee api route with jwt middleware

--- Employee Api/routes/api/employees.js
+++ Employee Api/routes/api/employees.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const {getAllEmployees, createNewEmployee, updateEmployee, deleteEmployee, getEmployee} = require('../../controllers/employeesController')
+const verifyJWT = require('../../middleware/verifyJWT')
 
 router.route('/')
-    .get(getAllEmployees)
+    .get(verifyJWT ,getAllEmployees)
     .post(createNewEmployee)
     .put(updateEmployee)
     .delete(deleteEmployee)
